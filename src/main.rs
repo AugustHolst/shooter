8741f2e6256ab2f2d256c3535e9f6f01bce35290
AugustHolst <AugustHeiningHolst@gmail.com>
2021-05-05 00:45:41 +0200

    Player rotates towards raycast from mouse

--- src/main.rs
+++ src/main.rs
@@ -20,22 +20,37 @@ fn spawn_player(
 	mut meshes: ResMut<Assets<Mesh>>,
 	mut materials: ResMut<Assets<StandardMaterial>>
 ){
-	
+	//spawn camera
 	commands.spawn_bundle(PerspectiveCameraBundle {
         transform: Transform::from_translation(Vec3::new(-5.0, 5.0, -5.0)).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
-    });
+		})
+		.insert(RayCastSource::<MyRaycastSet>::new());
+		
+	//spawn light
 	commands.spawn_bundle(LightBundle {
-            transform: Transform::from_xyz(-1.0, 0.0, 5.0),
-            ..Default::default()
-    });
+        transform: Transform::from_xyz(-1.0, 2.0, 5.0),
+        ..Default::default()
+		});
+	
+	//spawn planes
+	commands.spawn_bundle(PbrBundle {
+		mesh: meshes.add(Mesh::from(shape::Plane { size: 15.0 })),
+			material: materials.add(Color::WHITE.into()),
+			..Default::default()
+		})
+		.insert(Body::Cuboid { half_extends: Vec3::new(2.5, 0.0, 2.5) })
+		.insert(BodyType::Static)
+		.insert(RayCastMesh::<MyRaycastSet>::default());
+	
+	
 	//only need this here:
 	let player = commands.spawn()
 		.insert(Player)
 		.insert_bundle(PbrBundle {
 			mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-			material: materials.add(Color::WHITE.into()),
-			transform: Transform::from_translation(Vec3::ZERO),
+			material: materials.add(Color::TEAL.into()),
+			transform: Transform::from_translation(Vec3::new(0.0, 0.5, 0.0)),
 			..Default::default()
 		}).id();
 	println!("{}", player.id());
@@ -44,11 +59,18 @@ fn spawn_player(
 fn control_player(
 	mut commands: Commands,
 	mut ev_cursor_motion: EventReader<CursorMoved>,
-	mut query: Query<(&Player, &mut Transform)>
+	mut query: Query<(&Player, &mut Transform)>,
+	mut query_ray: Query<&mut RayCastSource<MyRaycastSet>>
 ){
 	if let Some(cursor) = ev_cursor_motion.iter().last() {
 		let (player, mut transform) = query.single_mut().expect("There is always a player");
-		transform.rotate(Quat::from_rotation_y(cursor.position.x));
+		for mut ray_src in &mut query_ray.iter_mut() {
+			ray_src.cast_method = RayCastMethod::Screenspace(cursor.position);
+			if let Some(intersection) = ray_src.intersect_top() {
+				let intersection_pos = intersection.1.position();
+				transform.look_at(intersection_pos, Vec3::Y);
+			}
+		}
 	}
 }
 
